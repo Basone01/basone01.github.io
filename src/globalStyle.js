a0d7b910f012e5fc1ccf7ec54aa8f6e44a427ec1
basone01 <basone01@hotmail.com>
2018-03-12 03:50:24 +0700

    added Pridi font and local font for offline  work

--- src/globalStyle.js
+++ src/globalStyle.js
@@ -2,10 +2,20 @@ import { injectGlobal } from 'styled-components';
 import 'normalize.css/normalize.css'
 
 injectGlobal`
+    /* @import url('https://fonts.googleapis.com/css?family=Pridi:400,700'); */
+    @font-face {
+        font-family: Pridi;
+        src: url(${require('./localFonts/Pridi-Regular.ttf')});
+    }
+    @font-face {
+        font-family: Pridi;
+        src: url(${require('./localFonts/Pridi-SemiBold.ttf')});
+        font-weight:bold;
+    }
     body{
         background-color:#222;
-        font-family:'Century Gothic';
-        font-weight:500;
+        font-family:'Pridi', serif;
+        color:#ddd;
         height:2000px;
         overflow:auto;
     }
