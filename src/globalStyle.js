6260319fda0df3f6413d37c294e49b982108779c
basone01 <basone01@hotmail.com>
2018-03-12 03:33:21 +0700

    remove example content , added normalize.css and injectedGlobalStyle

---
+++ src/globalStyle.js
@@ -0,0 +1,12 @@
+import { injectGlobal } from 'styled-components';
+import 'normalize.css/normalize.css'
+
+injectGlobal`
+    body{
+        background-color:#222;
+        font-family:'Century Gothic';
+        font-weight:500;
+        height:2000px;
+        overflow:auto;
+    }
+`
