0867f1daa38eade68d698cab5f315910601e51c3
basone01 <basone01@hotmail.com>
2018-03-14 05:11:19 +0700

    removed unused code

--- src/components/header/typingMessage/messageBox.js
+++ src/components/header/typingMessage/messageBox.js
@@ -4,7 +4,6 @@ import StyledMessageBox from './messageBox.styled'
 export default class MessageBox extends Component {
 	constructor(props) {
 		super(props);
-		const { index, msg } = props;
 		this.state = {
 			message: '',
 			timeout: null,
@@ -12,7 +11,7 @@ export default class MessageBox extends Component {
 	}
 
 	setupTypingAnimation() {
-		const { msg, index, wait, speed } = this.props;
+		const { msg, wait, speed } = this.props;
 		const timeout = msg.split('').map((c, i) => {
 			const snapshotMsg = msg.substr(0, i + 1);
 			return setTimeout(() => {
