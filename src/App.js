35301d43d4b24080c1feab15c25e561aa562ecff
basone01 <basone01@hotmail.com>
2018-03-13 16:44:38 +0700

    remove NavBar Children Content

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React, { Component,Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 import Header from './components/header/';
-import NavBar from './components/nav/'
+import NavBar from './components/nav/';
 class App extends Component {
 	render() {
-		return <Fragment>
-					<NavBar>sdkjfhkjsdfkj</NavBar>
-					<Header src={require("./img/headerBG.jpg")} msg="Hi, Welcome..." />
-				</Fragment>
-		
+		return (
+			<Fragment>
+				<NavBar />
+				<Header src={require('./img/headerBG.jpg')} msg="Hi, Welcome..." />
+			</Fragment>
+		);
 	}
 }
 
