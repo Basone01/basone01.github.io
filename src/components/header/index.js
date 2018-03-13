a61c8b4b737ea514efb3cdae28f07e119d643578
basone01 <basone01@hotmail.com>
2018-03-14 05:08:10 +0700

    change msg props to static string in header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { ImgContentBox } from './imgContentBox';
 import TypingMessageBox from './typingMessage/'
+
 export const Header = (props) => {
     return (
         <ImgContentBox src={props.src}>
-            <TypingMessageBox msg={props.msg}/>
+            <TypingMessageBox msg={"Hi, Welcome\nI am Basone01."}/>
         </ImgContentBox>
     );
 };
