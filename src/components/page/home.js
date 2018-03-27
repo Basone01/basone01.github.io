12734a0ab5568f4ad39c075972cc86f8a7b97736
basone01 <basone01@hotmail.com>
2018-03-28 03:05:08 +0700

    change separater between name and department and added label below profile pic edited some margin style

--- src/components/page/home.js
+++ src/components/page/home.js
@@ -29,7 +29,7 @@ const ProfilePic = styled.img`
 	width: 180px;
 	height: auto;
 	border: 5px solid var(--black);
-	margin-left: 1.5em;
+	margin-bottom: 1em;
 	@media only screen and (max-width:768px){
 	  width: 120px;
 	}
@@ -43,9 +43,16 @@ const AboutSection = (props) => (
         <Flex justifyContent="center">
             <Flex fluid grow={1} direction="column" alignItems="stretch">
                 <Flex fluid alignItems="baseline" justifyContent="center" style={{marginBottom: '1em'}}>
-                    <h3>Surachet Sangasaeng</h3>
-                    <h3 style={{padding: '0 0.1em'}}>|</h3>
-                    <p>Computer Science</p>
+                    <h3 style={{textAlign:"right"}}>Surachet Sangasaeng</h3>
+                    <span style={{
+                        width: 1,
+                        height:"2em",
+                        marginLeft:"1em",
+                        marginRight:"1em",
+                        alignSelf: "center",
+                        borderRight: '0.1em solid var(--white)'
+                    }}/>
+                    <p style={{textAlign:"left"}}>Computer Science</p>
                 </Flex>
                 <Flex fluid direction="column">
                     <HeadingText>About Me:</HeadingText>
@@ -53,15 +60,21 @@ const AboutSection = (props) => (
                         Hi, I'm a Computer-Science student from KMUTNB. I start my programming life from C language
                         while I was studying Matthayom 5 and I liked it since then,
                         because it is only one subject that I can prove all answer by myself.
-                        That's why decided to be a Computer-Science student. when I was studiying in the university,
+                        That's why decided to be a Computer-Science student.
+                    </Text>
+                    <Text indent justify>
+                        When I was studying in the university,
                         I met many programming language, many problem and got some skills form them,
                         And now I'm looking for an opportunity to use them to make something great.
                     </Text>
                 </Flex>
             </Flex>
-            <ProfilePic src={require('img/profilePic.jpg')}/>
+            <Flex fluid direction="column" alignItems="center" style={{marginLeft:"3em"}}>
+                <ProfilePic src={require('img/profilePic.jpg')}/>
+                <HeadingText center>THIS IS ME !!!</HeadingText>
+            </Flex>
         </Flex>
-        <Flex direction="column" alignItems="stretch" >
+        <Flex direction="column" alignItems="stretch">
             <HeadingText center>" You can see other's mistake more than your mistake, </HeadingText>
             <HeadingText center>and you can see other's bugs more than your bugs "</HeadingText>
         </Flex>
