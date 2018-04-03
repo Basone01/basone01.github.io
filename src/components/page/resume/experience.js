39415439952990b2379ed306a7bf6a267e025832
DESKTOP-OK4OG5A\Basone01 <basone01@hotmail.com>
2018-04-04 06:17:03 +0700

    added LineBreaker

--- src/components/page/resume/experience.js
+++ src/components/page/resume/experience.js
@@ -1,21 +1,21 @@
 import React, { Fragment } from 'react';
-import { HeadingText } from 'components/shared/';
+import { Flex, HeadingText, LineBreaker } from 'components/shared/';
 import { RightSideTopic } from './resume.styled';
+
 const ExperienceSide = () => {
     return (
         <Fragment>
             <RightSideTopic lg>EXPERIENCE</RightSideTopic>
             <HeadingText>Teacher Assistance :</HeadingText>
-            <HeadingText littleIndent small>
-                Computer Programming 1 ( Using Scratch, Python )
-            </HeadingText>
-
-            <HeadingText littleIndent small>
-                Object-Oriented Programming ( Using JAVA )
-            </HeadingText>
-            <HeadingText littleIndent small>
-                Basic Android Workshop ( Using JAVA )
-            </HeadingText>
+            <Flex fluid jc="flex-start" ali="flex-start">
+                <HeadingText littleIndent small>Computer Programming 1 <LineBreaker maxScreen={1056}/> ( Using Scratch, Python )</HeadingText>
+            </Flex>
+            <Flex fluid jc="flex-start" ali="flex-start">
+                <HeadingText littleIndent small>Object-Oriented Programming <LineBreaker maxScreen={1056}/>  ( Using JAVA )</HeadingText>
+            </Flex>
+            <Flex fluid jc="flex-start" ali="flex-start">
+                <HeadingText littleIndent small>Basic Android Workshop <LineBreaker maxScreen={1056}/>  ( Using JAVA )</HeadingText>
+            </Flex>
         </Fragment>
     );
 };
