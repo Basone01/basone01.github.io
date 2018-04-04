38b4aaa840ce9cd43f7ce560fdd9ee52b3f62ac0
DESKTOP-OK4OG5A\Basone01 <basone01@hotmail.com>
2018-04-05 01:15:11 +0700

    added Arrow Icon

--- src/fontAwesomeInit.js
+++ src/fontAwesomeInit.js
@@ -1,9 +1,18 @@
-import fontawesome from '@fortawesome/fontawesome'
-import brands from '@fortawesome/fontawesome-free-brands'
-import faHeadphones from '@fortawesome/fontawesome-free-solid/faHeadphones'
-import faCamera from '@fortawesome/fontawesome-free-solid/faCameraRetro'
-import faGamepad from '@fortawesome/fontawesome-free-solid/faGamepad'
-import faMail from '@fortawesome/fontawesome-free-solid/faEnvelope'
-import faPhone from '@fortawesome/fontawesome-free-solid/faPhoneSquare'
+import fontawesome from '@fortawesome/fontawesome';
+import brands from '@fortawesome/fontawesome-free-brands';
+import faHeadphones from '@fortawesome/fontawesome-free-solid/faHeadphones';
+import faCamera from '@fortawesome/fontawesome-free-solid/faCameraRetro';
+import faGamepad from '@fortawesome/fontawesome-free-solid/faGamepad';
+import faMail from '@fortawesome/fontawesome-free-solid/faEnvelope';
+import faPhone from '@fortawesome/fontawesome-free-solid/faPhoneSquare';
+import faArrowLeft from '@fortawesome/fontawesome-free-solid/faChevronCircleLeft';
 
-fontawesome.library.add(brands,faHeadphones,faCamera,faGamepad,faMail,faPhone)
+fontawesome.library.add(
+    brands,
+    faHeadphones,
+    faCamera,
+    faGamepad,
+    faMail,
+    faPhone,
+    faArrowLeft
+);
